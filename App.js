3ba9c40293c8dd4bfab54be45a63209123b1387e
Dre mf Randaci <drerandaci@gmail.com>
2018-03-10 23:43:21 -0600

    watson response working

--- App.js
+++ App.js
@@ -1,44 +1,107 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
+'use strict';
 import React, { Component } from 'react';
 import {
-  Platform,
+  AppRegistry,
+  Dimensions,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View
 } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      watson: []
+    };
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          SUP DUDE BRO MAN
-        </Text>        
+        <RNCamera
+            ref={ref => {
+              this.camera = ref;
+            }}
+            style = {styles.preview}
+            type={RNCamera.Constants.Type.back}
+            flashMode={RNCamera.Constants.FlashMode.on}
+            permissionDialogTitle={'Permission to use camera'}
+            permissionDialogMessage={'We need your permission to use your camera phone'}
+        />
+        <View><Text style={styles.response}>{this.state.watson}</Text></View>
+        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}}>
+        <TouchableOpacity
+            onPress={this.takePicture.bind(this)}
+            style = {styles.capture}
+        >
+            <Text style={{fontSize: 14}}> SNAP </Text>
+        </TouchableOpacity>
+        </View>
       </View>
     );
   }
+
+  takePicture = async function() {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const pic = await this.camera.takePictureAsync(options);
+      
+      this.setState({watson: pic.uri});
+
+      const data = new FormData();
+      data.append('name', 'testName'); 
+      data.append('photo', {
+        uri: pic.uri,
+        type: `image/${pic.type}`, 
+        name: 'testPhotoName'
+      });
+      
+      fetch(`https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key=&version=2016-05-20`, {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'multipart/form-data;'
+        },
+        body: data
+      }).then(res => {
+        console.log(res.json())
+      });
+
+      console.log(pic);
+    }
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'column',
+    backgroundColor: 'black'
   },
-  welcome: {
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20
+  },
+  response: {
+    flex: 0,
+    flexDirection: 'column',
+    backgroundColor: '#fff',
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    alignSelf: 'center',
+  }
 });
+
+AppRegistry.registerComponent('Watson', () => App);
