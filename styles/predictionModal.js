3c6ce773f25d5e460d4bdc6565de5f7072a33b69
Dre mf Randaci <drerandaci@gmail.com>
2018-03-26 12:26:54 -0500

    prediction modal stylesheet

---
+++ styles/predictionModal.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    scrollViewContainer: {
+        marginTop: 25,
+        marginBottom: 25
+    },
+    btnContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        paddingBottom: 12,
+    },
+    button: {
+        fontSize: 16
+    },
+});
+
+export default styles;
